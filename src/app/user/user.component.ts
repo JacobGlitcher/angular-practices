812c5842cd28b7d8acfc17c5a14ebd94d8d1c9ba
JacobGlitcher <hakob.yer@gmail.com>
2024-10-13 20:05:22 +0400

    feat: added ts, added getters, added event listener mockup

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.interface';
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,4 +11,20 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 
-export class UserComponent {}
+export class UserComponent {
+  private randomIndex: number = Math.floor(Math.random() * DUMMY_USERS.length);
+
+  selectedUser: User = DUMMY_USERS[this.randomIndex]
+
+  get avatarSrc(): string {
+    return `assets/users/${this.selectedUser.avatar}`;
+  }
+
+  get altText(): string {
+    return `${this.selectedUser.name} Avatar`;
+  }
+
+  onSelectUser(): void {
+    console.log('clicked!')
+  }
+}
