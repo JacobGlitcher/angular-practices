137271a9a282ea9a98ff3c915601d4b7e3b54c1d
JacobGlitcher <hakob.yer@gmail.com>
2024-10-17 22:50:11 +0400

    feat: adding dynamic naming for a user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,29 @@ import { Component } from '@angular/core'
 
 import { HeaderComponent } from './header/header.component'
 import { UserComponent } from './user/user.component'
+import { TasksComponent } from './tasks/tasks.component'
 import { DUMMY_USERS } from './dummy-users'
 import { User } from './user/user.interface'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   users: User[] = DUMMY_USERS
+  selectedUserId: string = ''
+
+  get userNameById(): string {
+    return (
+      this.users.find((user: User): boolean => user.id === this.selectedUserId)
+        ?.name || ''
+    )
+  }
 
   onSelectUser(id: string): void {
-    console.log(id)
+    this.selectedUserId = id
   }
 }
