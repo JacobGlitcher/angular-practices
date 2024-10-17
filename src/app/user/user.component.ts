c293b06c6fdaccaca792d517741d69ae644f7d3e
JacobGlitcher <hakob.yer@gmail.com>
2024-10-17 23:07:55 +0400

    refactor: made the Inputs shorter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
 
+import { User } from './user.interface'
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,20 +10,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
   styleUrl: './user.component.scss',
 })
 export class UserComponent {
-  @Input({ required: true }) avatar!: string
-  @Input({ required: true }) name!: string
-  @Input({ required: true }) id!: string
+  @Input({ required: true }) user!: User
   @Output() selectUser = new EventEmitter<string>()
 
   get avatarSrc(): string {
-    return `assets/users/${this.avatar}`
+    return `assets/users/${this.user.avatar}`
   }
 
   get altText(): string {
-    return `${this.name} Avatar`
+    return `${this.user.name} Avatar`
   }
 
   onSelectUser(): void {
-    this.selectUser.emit(this.id)
+    this.selectUser.emit(this.user.id)
   }
 }
